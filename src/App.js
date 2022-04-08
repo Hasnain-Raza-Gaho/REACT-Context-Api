0635a98a468939de075cde099cf27558399edcd2
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-04-08 11:51:24 +0500

    yes

--- src/App.js
+++ src/App.js
@@ -1,93 +1,38 @@
+import React, { Component } from 'react';
+export class App extends Component {
 
-import './App.css';
-import React from 'react';
-
-class App extends React.Component {
-
-  constructor() {
-    super()
-    this.state = {
-      value: 0,
-      data: [],
-      user: []
+  constructor(){
+    super();
+    this.state ={
+      count : 0
     }
-    console.log("first run constructor")  //=>not run again
+    console.log( 'consutructor run',this.state.count)
   }
-
-  componentDidMount() { //first time only run
-    console.log("third Run  componentDidMount")
-
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(json => this.setState({
-        data: json
-
-      }))
-
-    fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(response => response.json())
-      .then(json => this.setState({
-        data: [...this.data, json]
-
-      }))
-
-  }
-
-  shouldComponentUpdate() {
-    //not update state  
-    // firebase=>network
-    return true
-  }
-
-  static getDerivedStateFromProps(props, state) {  // CHECK state or Props 
-    console.log("Get Derived State From Props == >", state)
-    // return null 
-  }
-
-  getSnapshotBeforeUpdate(preprops, prestate) {  //State  update or props
-
-    //state before update
-    console.log(" getSnapshotBeforeUpdate:", prestate)
-
-  }
-
-  // componentDidUpdate(postprops, post, get) {
-  //   console.log("New Value", post)
+  
+  // getdata  (){
+  //   this.setState({
+  //     count: this.state.count+1
+  //   })
   // }
 
-
-
-
-
-
+  componentDidMount(){
+    console.log('componnetdidmount');
+  }
   render() {
-    console.log("second run render , " + this.state.data[0])
+    
 
+
+    console.log('render run')
     return (
       <div>
-        <h1>Hello{this.state.value}</h1>
-        <button onClick={() => this.setState({ value: this.state.value + 1 })}>INC</button>
-
-        <hr />
-        {this.state.data.map((v1, i) => {
-          //  console.log(v1)
-          return (
-            <div key={i}>
-              <b >user id : {v1.userId} </b>
-              <b>title : {v1.title}</b>
-              <hr />
-
-            </div>
-          )
-
-        })
-
-        }
-
+        <h1>life cycle methods..</h1>
+        <p>{this.state.count}</p>
+        <button >click</button>
+        {/* <button onClick={()=>this.getdata()}>click</button> */}
 
       </div>
     )
   }
 }
 
-export default App;
+export default App
